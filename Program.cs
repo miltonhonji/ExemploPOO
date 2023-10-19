9769f8879a713fe2dd1a213937b8ceb5d6f73d49
milton.honji <factorysolutionIt@outlook.com>
2023-10-19 12:59:06 -0300

    Finalizado POO

--- Program.cs
+++ Program.cs
@@ -5,8 +5,18 @@ Aluno a1 = new Aluno();
 
 a1.Nome = "Milton";
 a1.Idade = 37;
+a1.Email = "teste@teste.com";
+a1.Nota = 10;
+
 a1.Apresentar();
 
+Professor p1 = new Professor();
+
+p1.Nome = "Nome Professor";
+p1.Idade = 50;
+p1.Salario = 1000;
+
+p1.Apresentar();
 
 
 
